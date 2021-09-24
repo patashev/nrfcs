223ac257c3d12dd81263dd9d1a296714efface15
Pavel Tashev <pavel_t@mail.bg>
2021-09-24 12:30:58 +0300

    new file:   install.sh

---
+++ install.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/bash
+
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exec apt install git -y
+  if ![ -x "$(command --version gcc)" ]; then
+    echo 'Error: gcc is not installed.' >&2
+    exec apt install build-essential -y
+  fi
+fi
+
+
+function cloneResources()
+{
+    declare -a arr=(
+        "https://github.com/arut/nginx-rtmp-module.git" 
+        "git://git.openssl.org/openssl.git"
+        "https://github.com/kaltura/nginx-vod-module.git"
+        "https://github.com/vozlt/nginx-module-vts.git"
+        "https://github.com/apache/incubator-pagespeed-ngx.git"
+        );
+    for i in "${arr[@]}"
+    do
+        cd resources/
+        git clone $i
+        cd ../
+    done
+
+}
+
+
+function makeRepositories()
+{
+    export RESOURCES=resources;
+    declare -a arr=(
+        "$RESOURCES/pcre2-10.37"
+        "$RESOURCES/zlib-1.2.11"
+        );
+    for i in "${arr[@]}"
+    do
+        cd $i/
+        ./configure
+        make
+        make install
+        cd "../$RESOURCES/"
+    done
+
+}
+
+function makeNginx()
+{
+    cd resources/nginx-1.19.2
+    ./configure --with-http_ssl_module --with-http_geoip_module --prefix=/home/pafo/Documents/nginx-rtpm-stream-server/nginx --with-file-aio --with-http_stub_status_module --add-module=/home/pafo/Documents/nginx-rtpm-stream-server/nginx-rtmp-module --add-module=/home/pafo/Documents/nginx-rtpm-stream-server/nginx-vod-module --with-http_xslt_module --with-http_ssl_module --add-module=/home/pafo/Documents/nginx-rtpm-stream-server/nginx-module-vts --add-module=/home/pafo/Documents/nginx-rtpm-stream-server/incubator-pagespeed-ngx --with-http_xslt_module --with-http_secure_link_module --with-http_realip_module --with-http_gunzip_module --with-ipv6 --with-http_gzip_static_module
+}
+
+
+function downloadResources () 
+{
+    export RESOURCES=resources;
+    declare -a arr=(
+        "https://ftp.pcre.org/pub/pcre/pcre2-10.37.tar.gz"
+        "http://zlib.net/zlib-1.2.11.tar.gz"
+        "https://nginx.org/download/nginx-1.19.2.tar.gz"
+        );
+    for i in "${arr[@]}"
+    do
+        wget $i --directory-prefix="$RESOURCES" -O $RESOURCES/${i##*/}
+        wget --quiet --spider $i
+        if [ $? -eq 0 ] ; then    
+            wget $i --directory-prefix="$RESOURCES" -O $RESOURCES/${i##*/}
+        fi
+    done
+    FILES="$RESOURCES/*"
+    for f in $FILES
+    do
+        tar -xvf $f --directory "$RESOURCES/";
+    done
+    rm $RESOURCES/*.gz
+}
+
+
+if ! [ -x "$(ls -a | grep resources)" ]; then
+    #mkdir resources
+    #downloadResources
+    #cloneResources
+    #makeRepositories
+    makeNginx
+else
+    #downloadResources
+    #cloneResources
+    #makeRepositories
+    makeNginx
+fi
